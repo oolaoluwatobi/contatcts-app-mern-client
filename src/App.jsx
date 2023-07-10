0549bf69431946951ead2301794cd48447154fde
Olaoluwa Oladipo <58662625+oolaoluwatobi@users.noreply.github.com>
2023-07-10 17:02:54 +0100

    Update App.jsx layout again

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ import Favorite, {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/subscribers" element={<Layout />}>
-      <Route path="/subscribers"  element={<HomePage />} loader={homePageLoader} errorElement={<ErrorPage />} >
+      <Route path="."  element={<HomePage />} loader={homePageLoader} errorElement={<ErrorPage />} >
         <Route index element={<Index />} />
         <Route
           path="add"
