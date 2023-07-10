8431ef24665008fa05dbe4d910aed90886dd317c
Olaoluwa Oladipo <58662625+oolaoluwatobi@users.noreply.github.com>
2023-07-10 18:43:27 +0100

    uu

--- src/api/users.jsx
+++ src/api/users.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:3500/"
-  baseURL: "https://mern-contacts-app.onrender.com/"
+  baseURL: "https://mern-contacts-app.onrender.com/subscribers"
   // baseURL: "mongodb+srv://oolaoluwatobi:123ola@cluster0.g928usv.mongodb.net/usersdb?retryWrites=true&w=majority"
 });
