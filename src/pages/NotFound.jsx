eaaf1e5d43b1b0f97510d709346063d9a323df78
Olaoluwa Oladipo <58662625+oolaoluwatobi@users.noreply.github.com>
2023-07-10 10:22:45 -0700

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className='mx-5'>
       <h1>Oops!</h1>
       <h1 className='text-4xl font-bold'>Sorry the page you are looking for was not found.</h1>
-      <Link className='bg-[#161616] text-white  mt-5 py-2 rounded flex justify-center' to={'/subscribers'}>
+      <Link className='bg-[#161616] text-white  mt-5 py-2 rounded flex justify-center' to={'subscribers'}>
         <button className=''>Return to Home</button>
       </Link>
     </div>
